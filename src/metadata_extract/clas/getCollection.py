2f198554b8901f4748a6d8a7967f5297aad2c05f
reck <Christoph.Reck@dlr.de>
2023-06-19 12:42:32 +0000

    Enahnced getCollection.py to support again "L0", "L1" and "L2" in filenames, fixed S2-MSI identification

--- src/metadata_extract/clas/getCollection.py
+++ src/metadata_extract/clas/getCollection.py
@@ -1,6 +1,6 @@
 import os
 
-levels = {'0':'L0', '1':'L1', '2':'L2', '1A':'L1A', '1B':'L1B', '1C':'L1C', '2A':'L2A', '3A':'L3A'}
+levels = {'0':'L0', 'L0':'L0', '1':'L1', 'L1':'L1', '2':'L2', 'L2':'L2', '1A':'L1A', '1B':'L1B', '1C':'L1C', '2A':'L2A', '3A':'L3A'}
 S3_types = { 'OL':'OLCI', 'SR':'SRAL', 'SL':'SLSTR', 'SY':'Synergy', 'DO':'DORIS', 'MW':'MWR', 'AX':'AUX', 'GN':'GNSS' , 'TM':'TM'}
 def collection(filename, productType):
     sensor = os.path.basename(filename)[0:2]
@@ -8,7 +8,7 @@ def collection(filename, productType):
         level = levels.get(productType[8:9], 'AUX')
         collection = 'S1.AUX' if (productType[0:2] in ['GP','HK']) or (level == 'AUX') else 'S1.SAR.'+level
     elif sensor == 'S2':
-        level = levels.get(productType[5:7], 'AX') if productType[0:3] == 'MSI' else 'AUX'
+        level = levels.get(productType[4:6], 'AX') if productType[0:3] == 'MSI' else 'AUX'
         collection = 'S2.AUX' if level == 'AUX' else 'S2.MSI.'+level
     elif sensor == 'S3':
         level = 'AUX' if productType[9:11] == 'AX' else levels.get(productType[3:4], 'AUX')
